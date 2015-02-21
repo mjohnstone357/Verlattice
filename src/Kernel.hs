31d39f0ede240271446da7061c393aa3e6ea4eef
Matthew Johnstone <mjohnstone357@gmail.com>
2015-02-21 14:43:02 +0000

    More experimenting with the Kernel structure.

--- src/Kernel.hs
+++ src/Kernel.hs
@@ -3,62 +3,29 @@ module Kernel where
 
 -- Generic kernel stuff
 
-data Kernel l s = Kernel {
-  kernelLogic :: l,
-  kernelState :: s
-}
-
-data KernelTree l s = KernelTree (Kernel l s) [KernelTree l s]
-
-data CommandResult s r = CommandResult {
-  resultantState :: s,
-  response :: r
-} deriving (Show)
-
-data ClientFunction i o = ClientFunction {
-  input :: i,
-  output :: o,
+data Kernel i o = Kernel {
+  kernelID :: String,
   function :: i -> o
 }
+
+data KernelTree i o = KernelTree (Kernel i o) [KernelTree i o]
+  
+applyToKernel :: (Show i, Show o) => i -> Kernel i o -> o
+applyToKernel input kernel = (function kernel) input
+
+--applyToKernelTree :: (Show i, Show o) => i -> KernelTree i o -> o
 
 -- TODO The implementation must provide an interface where an object is passed and another is returned
 
 -- Implementation-specific stuff
 
-data MyInputType = NR | ASR | GSCR
-data OutputType = NResp | ASResp | GSCResp
+data MyInput = InputOne | InputTwo | InputThree
+data MyOutput = OutputOne | OutputTwo | OutputThree
 
-data MyKernelState = MyKernelState {
-  xs :: [String]
-} deriving (Show)
+myKernel :: Kernel MyInput MyOutput
+myKernel = Kernel{kernelID = "test kernel", function = myKernelLogic}
 
-data NullRequest = NullRequest
-data AppendStringRequest = AppendStringRequest
-data GetStringCountRequest = GetStringCountRequest
-
-data NullResponse = NullResponse
-                  deriving (Show)
-data AppendStringResponse = AppendStringSuccess
-data GetStringCountResponse = StringCount Int
-                 deriving (Show)
-
-data MyKernelLogic = MyKernelLogic {
-  pass :: MyKernelState -> CommandResult MyKernelState NullResponse,
-  appendString :: String -> MyKernelState -> CommandResult MyKernelState NullResponse,
-  getStringCount :: MyKernelState -> CommandResult MyKernelState GetStringCountResponse
-}
-
-myKernelState :: MyKernelState
-myKernelState = MyKernelState {
-  xs = ["foo", "bar"]
-}
-
-myKernelLogic :: MyKernelLogic
-myKernelLogic = MyKernelLogic {
-  pass = \state -> CommandResult{resultantState = state, response = NullResponse},
-  appendString = \x state -> CommandResult{resultantState = state{xs = x : (xs state)}, response = NullResponse},
-  getStringCount = \state -> CommandResult{resultantState = state, response = StringCount (length (xs state))}
-}
-
-myInstance :: Kernel MyKernelLogic MyKernelState
-myInstance = Kernel {kernelLogic = myKernelLogic, kernelState = myKernelState}
+myKernelLogic :: MyInput -> MyOutput
+myKernelLogic InputOne = OutputOne
+myKernelLogic InputTwo = OutputTwo
+myKernelLogic InputThree = OutputThree
