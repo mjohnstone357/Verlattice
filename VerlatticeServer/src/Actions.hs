c48f047feafe3dc70c8be4df757b10b2f77d97ef
Matthew Johnstone <mjohnstone357@gmail.com>
2015-05-03 20:20:15 +0100

    Intial JSON parsing for Actions.

--- VerlatticeServer/src/Actions.hs
+++ VerlatticeServer/src/Actions.hs
@@ -1,6 +1,8 @@
 
 module Actions where
 
+import Text.JSON
+
 data Action = Action {
   actionName :: String,
   inputs :: [ActionInput],
@@ -17,5 +19,31 @@ data ActionOutput = ActionOutput {
   outputQuantity :: Int
 } deriving (Eq, Read, Show, Ord)
 
---instance JSON 
+instance JSON ActionInput where
+  showJSON actionInput =
+    let name = inputResourceName actionInput :: String;
+        quantity = show $ inputQuantity actionInput :: String;
+        resourcesJSON = toJSObject [("inputResourceName", JSString (toJSString name)),
+                                    ("inputQuantity", JSString (toJSString quantity))] in
+    JSObject resourcesJSON
 
+  readJSON (JSObject o) =
+    let pairs = (fromJSObject o) :: [(String, JSValue)];
+        (Just jsName) = (lookup "inputResourceName" pairs) :: Maybe JSValue;
+        (Just jsQuantity) = lookup "inputQuantity" pairs :: Maybe JSValue;
+        (JSString jsStringName) = jsName;
+        (JSString jsStringQuantity) = jsQuantity
+    in
+     Ok ActionInput{inputResourceName = fromJSString jsStringName,
+                 inputQuantity = (read (fromJSString jsStringQuantity)) :: Int}
+  readJSON _ = Error "invalid json type"
+
+
+exampleActionInput :: ActionInput
+exampleActionInput = ActionInput {
+  inputResourceName = "foo",
+  inputQuantity = 500
+}
+
+exampleJSON :: String
+exampleJSON = "{\"inputResourceName\":\"foo\",\"inputQuantity\":\"500\"}"
